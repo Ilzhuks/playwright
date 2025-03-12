16cfb16ceae207ce60b359f20140d04039b9ab54
Ilzhuks <ilze.vilcina7@gmail.com>
2025-03-12 22:12:12 +0200

    edited code

--- tests/registrationSuccessful.spec.ts
+++ tests/registrationSuccessful.spec.ts
@@ -1,15 +1,14 @@
 import { test, expect } from '@playwright/test';
 
+
 test('Validate successful registration', async ({ page }) => {
     await page.goto('https://auth-home-task.vercel.app/')
     await page.getByText('Go to Registration').click()
-    await page.waitForTimeout(1000)
+
 
     // Validate that is opened Registration Form
     const heading = await page.locator('h1')
-    const headingText = await heading.innerText()
-    console.log(headingText)
-    expect(headingText).toContain('Registration Form')
+    await expect(heading).toHaveText('Registration Form')
 
     // Added input data to each field with valid data for successful registration
     await page.locator('input[name="username"]').type('Test')
@@ -19,12 +18,10 @@ test('Validate successful registration', async ({ page }) => {
 
     // Click buttom Register
     await page.getByText('Register').click()
-    await page.waitForTimeout(1000)
+
 
     // Validate that user is registraded successfully by checking the message that appears if user is registrated
     const successMessage = page.locator('p.text-green-500.mb-4')
-    const messageText = await successMessage.innerText()
-    console.log(messageText)
-    expect(messageText).toBe('You have registered successfully!')
+    await expect(successMessage).toHaveText('You have registered successfully!')
 
 })
